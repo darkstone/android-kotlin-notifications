3013b7e0840437b9532d40e8e087707b785a5026
Andries <Andriesandries.spies@gmail.com>
2019-11-17 23:03:28 +0200

    Explored remote messaging payloads and services.

--- app/src/main/java/com/example/android/eggtimernotifications/messaging/MessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/messaging/MessagingService.kt
@@ -2,12 +2,18 @@ package com.example.android.eggtimernotifications.messaging
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refresh token token: $token")
         sendRegistrationToServer(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "Remote message from ${remoteMessage.from}")
+        remoteMessage.data.let { payload -> Log.d(TAG, "Message payload: $payload") }
     }
 
     private fun sendRegistrationToServer(token: String) {
